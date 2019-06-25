76b7a2048828a5b50fef5d77e95e6169ae0a64f3
Mushvig Niftaliyev <mniftaliyev@Mushvigs-MacBook-Pro.local>
2019-06-25 10:18:13 +0400

    Added blogs

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,9 +6,29 @@ class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
+
+  renderList() {
+    let listCount = 0;
+    return this.props.posts.map(e => {
+      if(listCount < 10){
+        listCount++;
+      return (
+        <div className="item" key={e.id} style={{"marginBottom":"20px"}}>
+          <i className="large middle aligned icon user" />
+          <div className='content'>
+            <div className='description'>
+              <h2>{e.title}</h2>
+              <p>{e.body}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    })
+  }
+
   render() {
     console.log(this.props.posts);
-    return <div>Post List</div>;
+    return <div className='ui relaxed divided list'>{this.renderList()}</div>;
   }
 }
 
